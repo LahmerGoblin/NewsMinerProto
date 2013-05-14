b30361ec513f672de9ed9d3ded8659c56bbe8f06
DieMJ <fideldimjdum@yahoo.de>
2013-05-14 12:59:37 +0200

    play 2.1.1

--- RSSFeed/app/controllers/Application.scala
+++ RSSFeed/app/controllers/Application.scala
@@ -5,35 +5,8 @@ import play.api.mvc._
 
 object Application extends Controller {
   
-	def index = Action {
-		Redirect(routes.Application.feeds)
-	}
-	import models.Feed
-	
-	def feeds =  Action {
-		Ok(views.html.index(Feed.all(), feedForm))
-	}
-  
-	def newFeed = Action { implicit request =>
-		feedForm.bindFromRequest.fold(
-			errors => BadRequest(views.html.index(Feed.all(), errors)),
-			URL => {
-				Feed.create(URL)
-				Redirect(routes.Application.feeds)
-			}
-		)
-	}
-	
-	def deleteFeed(id: Long) = Action {
-		Feed.delete(id)
-		Redirect(routes.Application.feeds)
-	}
-  
-	import play.api.data._
-	import play.api.data.Forms._
-
-	val feedForm = Form(
-		"URL" -> nonEmptyText
-	)
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
   
 }
--- RSSFeed/project/Build.scala
+++ RSSFeed/project/Build.scala
@@ -4,11 +4,11 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "add_RSSFeed"
+  val appName         = "RSSFeed"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.apache.derby" % "derby" % "10.10.1.1",
+    // Add your project dependencies here,
     jdbc,
     anorm
   )
